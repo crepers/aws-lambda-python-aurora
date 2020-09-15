6cac3c663faa361901017c30e7822d6f0d93f6a4
Jung <woochul@909c4aba0131.ant.amazon.com>
2020-09-15 20:33:20 +0900

    first commit.

---
+++ rds_config.py
@@ -0,0 +1,5 @@
+#config file containing credentials for RDS MySQL instance
+db_username = "admin"
+db_password = "ug2admin"
+db_name = "ug2database" 
+db_host = "xxxx.ap-northeast-2.rds.amazonaws.com"
